7061f29055c5731769322b4f7a93cc9c16c97c41
Alberthaciverdiyev1 <7cw5lnl@code.edu.az>
2023-03-03 22:57:26 +0400

    Endirim

---
+++ C# projects/Endirim/Endirim/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Endirim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mebleg = -6;
+            if (mebleg>0 && mebleg < 100)
+            {
+                Console.WriteLine("5% endirim");
+
+            }
+            else if (mebleg < 1000 && mebleg > 100)
+                       {
+                Console.WriteLine("8% endirim");
+            }
+            else if (mebleg > 1000) { 
+
+                Console.WriteLine("10% endirim");
+            }
+            else
+            {
+                Console.WriteLine("0% endirim");
+            }
+        }
+    }
+}
---
+++ JS projects/Endirim.js
@@ -0,0 +1,13 @@
+let a = -2000;
+if (a > 0 && a < 100) {
+    console.log("5 % endirim ");
+}
+else if (a >= 100 && a <= 1000) {
+    console.log("8 % endirim ");
+}
+else if (a > 1000) {
+    console.log("10 % endirim");
+}
+else {
+    console.log("0 % endirim")
+}
